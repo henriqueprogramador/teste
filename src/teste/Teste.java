22b71dab14d80cbeef58bb20a3abcadd751d2997
henriqueprogramador <haugusto455@gmail.com>
2021-01-15 14:07:23 -0300

    Add files via upload

---
+++ src/teste/Teste.java
@@ -0,0 +1,21 @@
+
+package teste;
+
+public class Teste {
+
+    
+    public static void main(String[] args) {
+        int a, b, x, y;
+a = 3;
+b = -2;
+x = -1;
+y = 6;
+x += b;
+y /= a;
+b -- ;
+System.out.println(x);
+System.out.println(y);
+System.out.println(b);
+    }
+    
+}
